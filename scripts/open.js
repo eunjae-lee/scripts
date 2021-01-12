1fb7f06054a8db09a8e47ce1fcac2454d86f38b1
eunjae-lee <karis612@gmail.com>
2021-01-12 14:48:23 +0100

    add open

---
+++ scripts/open.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+const { exec } = require("shelljs");
+const applescript = require("applescript");
+
+function showNotification(title) {
+  const script = `
+    display notification with title "${title}"
+  `;
+  applescript.execString(script);
+}
+
+const url = exec("pbpaste", { silent: true }).toString().trim();
+
+if (url.startsWith("https://github.com/") && url.includes("/pull/")) {
+  const [, , , , repo, , number] = url.split("/");
+  if (exec(`cd ~/workspace/${repo} && git status --porcelain`).code !== 0) {
+    showNotification("The working directory is not clean.");
+  } else {
+    exec(`cd ~/workspace/${repo} && code . && hub pr checkout ${number}`);
+  }
+}
